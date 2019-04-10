9689be275dbbcc191e7fbc04e93669fc08942444
Ezios <eziosyanse@gmail.com>
2019-04-10 20:09:26 +0800

    add typing for configure

--- src/request/axios.ts
+++ src/request/axios.ts
@@ -1,19 +1,19 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
 
-export type IHandleReponse = <T>(
+export type IHandleReponse<T> = (
   response: AxiosResponse<T>
 ) => [boolean, AxiosResponse<T>]
 export type IHandleError = (error: AxiosError) => void
 
-let handleResponse: IHandleReponse = response => [true, response]
+let handleResponse: IHandleReponse<any> = response => [true, response]
 let handleError: IHandleError = () => {}
 
 export const defaultsConfig: AxiosRequestConfig = {
   timeout: 10000,
 }
 
-export const configure = (handles: {
-  handleResponse?: IHandleReponse
+export const configure = <T>(handles: {
+  handleResponse?: IHandleReponse<T>
   handleError?: IHandleError
 }) => {
   if (handles.handleResponse) {
